dfb6f676c02c9e9fc5d7d35637ae311eab97b530
Devin Lane <devin.c.lane@gmail.com>
2021-06-24 20:54:31 -0700

    add script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.getElementById("count-el").innerText = 5; 
+
+let count = 0;
+
+count += 1;
+
+console.log(count);
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints += 50;
+console.log(bonusPoints);
+
+bonusPoints -= 75;
+console.log(bonusPoints);
+
+bonusPoints += 45;
+console.log(bonusPoints);
+
