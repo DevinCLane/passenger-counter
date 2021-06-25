e30aa6dedf71995d18f84ff16fd8bc4eb0b353f3
Devin Lane <devin.c.lane@gmail.com>
2021-06-24 20:58:27 -0700

    add increment button

--- script.js
+++ script.js
@@ -6,15 +6,4 @@ count += 1;
 
 console.log(count);
 
-let bonusPoints = 50;
-console.log(bonusPoints);
-
-bonusPoints += 50;
-console.log(bonusPoints);
-
-bonusPoints -= 75;
-console.log(bonusPoints);
-
-bonusPoints += 45;
-console.log(bonusPoints);
 
