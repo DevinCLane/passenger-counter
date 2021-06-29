e7fef5c9904fc70da2fe359745924127450503b6
Devin Lane <devin.c.lane@gmail.com>
2021-06-29 16:19:28 -0700

    add increment logic

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// document.getElementById("count-el").innerText = 5; 
-
+// store the count
 let count = 0;
 
-count += 1;
-
-console.log(count);
-
+// when a user clicks the button, increment the count by 1
+document.getElementById("increment-btn").onclick = function addPassenger() {
+    count++;
+    document.getElementById("count-el").innerText = count;
+}
 
